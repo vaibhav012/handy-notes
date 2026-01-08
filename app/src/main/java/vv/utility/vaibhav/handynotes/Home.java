90857eb00ed23d688ad74211b5e8d18190fd71df
Vaibhav <vaibhav.tayal95@gmail.com>
2026-01-08 20:27:38 +0530

    remove account related activities

--- app/src/main/java/vv/utility/vaibhav/handynotes/Home.java
+++ app/src/main/java/vv/utility/vaibhav/handynotes/Home.java
@@ -1,17 +1,8 @@
 package vv.utility.vaibhav.handynotes;
 
-import android.app.ProgressDialog;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.preference.PreferenceManager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
@@ -26,12 +17,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.lang.String;
 
 public class Home extends AppCompatActivity implements CustomAdapter.TalkToActivity{
 
